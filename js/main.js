4172795dec198daede1d036308d295c22749cc4b
JIN44638 <hoyajs77@gmail.com>
2025-08-06 12:18:07 +0900

    nav자바스크립트 연결

---
+++ js/main.js
@@ -0,0 +1,14 @@
+window.addEventListener("load", function () {
+// 각각의 요소를 변수에 담아놓는다
+    
+  const nav = document.querySelector(".nav");
+  const headerBg = document.querySelector(".header-bg");
+  // nav에 마우스 엔터 했을때 즉 호버했을때
+  nav.addEventListener("mouseenter", function () {
+    headerBg.style.height = "400px";
+  });
+   // nav에 마우스 똇을때 즉 호버를 안했을때
+   nav.addEventListener("mouseleave", function () {
+    headerBg.style.height = "0px";
+  });
+});
